7cf531758c3a5df3b80b494ef7937c8e599a87ad
choys0710 <choys0710@naver.com>
2017-11-10 09:48:42 +0900

    test

---
+++ MyFirstApp/timeList.swift
@@ -0,0 +1,16 @@
+//
+//  timeList.swift
+//  MyFirstApp
+//
+//  Created by 조윤성 on 2017. 11. 10..
+//  Copyright © 2017년 Joonsang Yoo. All rights reserved.
+//
+
+import Foundation
+
+class timeList {
+    
+    var hhmm: String = String()
+    var closed: String = String()
+    
+}
